aadac2de14c66bc97cf7d64ff98a31d56fdf4241
Douglas Soares <douglassoares.cinema@gmail.com>
2024-09-17 03:41:21 -0300

    Primeira Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Alfabeto em letras maiúsculas:\n");
+
+        for (int i = 65; i <= 90; i++){
+            Console.Write((char)i + " ");
+        }
+
+        int referenciaASCII = 90;
+        Console.WriteLine("\n");
+        Console.WriteLine($"O valor de {(char)referenciaASCII} é {referenciaASCII}");
+
+        Console.WriteLine("\nAlfabeto em letras minúsculas:\n");
+
+        for (int i = 97; i <= 122; i++){
+            Console.Write((char)i + " ");
+        }
+
+        referenciaASCII = 122;
+        Console.WriteLine("\n");
+        Console.WriteLine($"O valor de {(char)referenciaASCII} é {referenciaASCII}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Alfabeto em LOOP
+## Entendendo o tipo "char"
+&nbsp;
+
+Um programa simples que demonstra veementemente que **char** é um tipo inteiro. Os loops usam os números de referências de A,Z,a e z na Tabela ASCII para imprimir todas as letras do alfabeto na tela, em maiúsculas e minúsculas.
